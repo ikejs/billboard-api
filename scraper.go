4dc3618cb96455b54a7da68dde6ac569a9295434
Ike <ikescode@gmail.com>
2021-02-11 16:30:11 -0600

    scrape song data, print songs

---
+++ scraper.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	c.OnHTML(".chart-element__information", func(e *colly.HTMLElement) {
+		// Find song using an attribute selector
+		// Matche elements by class
+		title := e.ChildText(".chart-element__information")
+		artist := e.ChildText(".text--truncate")
+
+		// Print song info
+		fmt.Printf("Song found: %q -> %s\n", title, artist)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited", r.Request.URL)
+	})
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
+
+	c.Visit("https://www.billboard.com/charts/hot-100")
+}
