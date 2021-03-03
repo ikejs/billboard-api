f99ea024f144db481611cf5f117102bbe60554f3
Ike <ikescode@gmail.com>
2021-03-02 21:56:47 -0600

    fix class targeting, export data to JSON

--- scraper.go
+++ scraper.go
@@ -2,57 +2,62 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gocolly/colly"
 )
 
-type song struct {
-	title  string
-	artist string
+type Song struct {
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+	Rank   string `json:"rank"`
 }
 
-func scrapeSongs() []song {
+func scrapeSongs() []Song {
 	// Instantiate default collector
 	c := colly.NewCollector()
-	songs := []song{}
+	allSongs := []Song{}
 
-	c.OnHTML(".chart-element__information", func(e *colly.HTMLElement) {
+	c.OnHTML(".chart-list__element", func(e *colly.HTMLElement) {
 		// Find song using an attribute selector
 		// Matche elements by class
-		title := e.ChildText(".chart-element__information")
-		artist := e.ChildText(".text--truncate")
+		title := e.ChildText(".chart-element__information__song")
+		artist := e.ChildText(".chart-element__information__artist")
+		rank := e.ChildText(".chart-element__rank__number")
 
-		// Print song info
-		// fmt.Printf("Song found: %q -> %s\n", title, artist)
 		// add songs to list
-		songs = append(songs, song{title, artist})
+		allSongs = append(allSongs, Song{
+			Title:  title,
+			Artist: artist,
+			Rank:   rank,
+		})
 	})
 
 	c.Visit("https://www.billboard.com/charts/hot-100")
-	return songs
+	return allSongs
 }
 
-func renderSongs(w http.ResponseWriter, r *http.Request) {
-	js, err := json.Marshal(scrapeSongs())
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+var songs []Song
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+func renderSongsHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(songs)
 }
 
-func handleRequests() {
-	http.HandleFunc("/", renderSongs)
+func createFile(data []Song) {
+	file, _ := json.MarshalIndent(data, "", " ")
+	_ = ioutil.WriteFile("songs.json", file, 0644)
+}
+
+func startServer() {
+	songs = scrapeSongs()
+	createFile(songs)
+	http.HandleFunc("/", renderSongsHandler)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
-
 	// API
-	handleRequests()
-
+	startServer()
 }
